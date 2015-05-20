ad2299ed7c62b90aa9067fe234059d6a04f28926
Jon Childress <jon@childr.es>
2015-05-20 16:15:58 -0500

    cleanup .cabal and Main.hs files

--- src/Main.hs
+++ src/Main.hs
@@ -1,43 +1,29 @@
------------------------------------------------------------------------------
---
--- Module      :  Main
--- Copyright   :
--- License     :  GPL-2
---
--- Maintainer  :  jnf@arcor.de
--- Stability   :
--- Portability :
---
--- |
---
------------------------------------------------------------------------------
+module Main (main) where
 
-module Main where
+import System.Console.GetOpt (usageInfo)
+import System.Environment (getArgs)
+import System.Exit (exitSuccess)
+import Debug.Trace (trace)
+import Text.PrettyPrint (render)
 
+import qualified Text.Parsec as PP (runParser)
+import qualified Text.Parsec.Error as PP
 
 import IDL.AST
 import IDL.Parser
 import IDL.Printer
 
-import System.Console.GetOpt (usageInfo)
-import System.Environment (getArgs)
-import System.Exit (exitSuccess)
-import qualified Text.Parsec as PP (runParser)
-import qualified Text.Parsec.Error as PP
-       (errorMessages, messageString)
-import Debug.Trace (trace)
-import Text.PrettyPrint (render)
-
 main :: IO ()
-main = do
-    args <- getArgs
-    fp <- case args of
-            [] ->  do
-                putStrLn $ "Generator requires input idl file"
-                exitSuccess
-            (hd:_) -> return (hd)
-    string <- readFile fp
-    case PP.runParser idlParser () "" string of
-        Left error -> mapM_ (putStrLn . PP.messageString) (PP.errorMessages error)
-        Right idl -> putStr (render (ppPureScriptFFI idl))
+main = getArgs >>= getFilepath >>= readFile >>= runParser
 
+getFilepath :: [String] -> IO String
+getFilepath (fp:_) = return fp
+getFilepath _ = do
+    putStrLn $ "Generator requires input IDL file"
+    exitSuccess
+
+runParser :: String -> IO ()
+runParser body =
+    case PP.runParser idlParser () "" body of
+      Left err  -> mapM_ (putStrLn . PP.messageString) (PP.errorMessages err)
+      Right idl -> putStr . render $ ppPureScriptFFI idl
