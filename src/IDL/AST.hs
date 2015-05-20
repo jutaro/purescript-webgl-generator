efe23f8e91ba5d991ff65f55ea121a7d43ed2990
Jon Childress <jon@childr.es>
2015-05-20 16:33:02 -0500

    clean up AST

--- src/IDL/AST.hs
+++ src/IDL/AST.hs
@@ -1,60 +1,47 @@
------------------------------------------------------------------------------
---
--- Module      :  IDL.AST
--- Copyright   :
--- License     :  GPL-2
---
--- Maintainer  :  jnf@arcor.de
--- Stability   :
--- Portability :
---
--- |
---
------------------------------------------------------------------------------
-
 module IDL.AST where
 
 type Idl = [Decl]
 
 isEnum :: Decl -> Bool
-isEnum Enum{}  = True
-isEnum _       = False
+isEnum Enum{} = True
+isEnum _      = False
 
 isFunction :: Decl -> Bool
-isFunction Function{}  = True
-isFunction _       = False
+isFunction Function{} = True
+isFunction _          = False
 
-data Decl =
-  Enum
-    { enumName  :: String
-    , enumValue :: Integer
-    }
-  |
-  Comment
-      { comment  :: String
+data Decl
+  = Enum
+      { enumName  :: String
+      , enumValue :: Integer
+      }
+  | Comment
+      { comment :: String
       }
   | Function
-    { methodName :: String
-    , methodRetType :: Type
-    , methodArgs :: [Arg]
-    , methodRaises :: Maybe String
-    }
+      { methodName    :: String
+      , methodRetType :: Type
+      , methodArgs    :: [Arg]
+      , methodRaises  :: Maybe String
+      }
   | Attribute
-    { attIsReadonly :: Bool
-    , attType :: Type
-    , attName :: String
-    }
+      { attIsReadonly :: Bool
+      , attType       :: Type
+      , attName       :: String
+      }
   deriving (Eq,Show)
 
-data Type =
-    Type { typeName :: String
-         , typeIsArray :: Bool
-         , typeCondPara :: Maybe String}
+data Type
+  = Type
+      { typeName     :: String
+      , typeIsArray  :: Bool
+      , typeCondPara :: Maybe String
+      }
   deriving (Eq,Show)
 
-data Arg =
-    Arg {argType :: Type,
-         argName :: String}
+data Arg
+  = Arg
+      { argType :: Type
+      , argName :: String
+      }
   deriving (Eq,Show)
-
-
